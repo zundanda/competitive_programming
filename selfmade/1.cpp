f27414303fcef9ebacda3d86829fcd27913cc835
zunda <34739255+zundanda@users.noreply.github.com>
2020-03-16 19:55:26 +0900

    Update 1.cpp

--- selfmade/1.cpp
+++ selfmade/1.cpp
@@ -8,18 +8,18 @@ using ll = long long;
 int main (){
     int N;
     cin>>N;
-    bool isvalid[N+1]; //ip^2の形でないものがisvalidでtrueになる.
-    fill(isvalid,isvalid+N+1,true);
+    bool square_free[N+1]; //square_freeなものはtrueになる.
+    fill(square_free,square_free+N+1,true);
     for(int p=2;p*p<=N;p++){
         for(int i=1;i*p*p<=N;i++){
-            isvalid[i*p*p]=false;
+            square_free[i*p*p]=false;
         }
     }
     ll ans=0;
     for(int i=1;i<=N;i++){
-        if(isvalid[i]){
+        if(square_free[i]){
             for(int j=1;i*j*j<=N;j++){
-                int k=sqrt(N/i)-j; //sqrt(i)(j+k)<=sqrt(N)を満たすkの個数を数える. 計算より1<=k<=sqrt(N/i)-jを満たす整数kの個数である.
+                int k=sqrt(N/i)-j; //sqrt(i)(j+k)<=sqrt(N)を満たすkの個数を数える.
                 ans+=k;
             }
         }
