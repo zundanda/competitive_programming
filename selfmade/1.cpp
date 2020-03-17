56c3f0b740fd907af1c4f0b668580b7930a8ec42
zunda <34739255+zundanda@users.noreply.github.com>
2020-03-17 11:53:53 +0900

    Update 1.cpp

--- selfmade/1.cpp
+++ selfmade/1.cpp
@@ -7,18 +7,18 @@ using ll = long long;
 int main (){
     int N;
     cin>>N;
-    bool square_free[N+1]; //square_freeなものはtrueになる.
+    bool square_free[N+1]; 
     fill(square_free,square_free+N+1,true);
     for(int p=2;p*p<=N;p++){
         for(int i=1;i*p*p<=N;i++){
-            square_free[i*p*p]=false;
+            square_free[i*p*p]=false; //平方因子を持つ整数をfalseにする.
         }
     }
-    ll ans=0;
+    int ans=0;
     for(int i=1;i<=N;i++){
-        if(square_free[i]){
-            int k=sqrt(N/i); //sqrt(i)(j+k)<=sqrt(N)を満たすkの個数.
-            ans+=((k*(k-1))/2);
+        if(square_free[i]){//平方因子を持たない整数iに対し
+            int m=sqrt(N/i); 
+            ans+=((m*(m-1))/2); //sqrt(i)(j+k)<=sqrt(N)を満たす(j,k)の個数を数える.
         }
     }
     cout<<ans<<endl;
